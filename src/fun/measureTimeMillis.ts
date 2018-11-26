6064173757211f6d13082003a27bca8c64ba9d54
kobi <kobi.b@inner-active.com>
2018-11-26 13:37:59 +0200

    feat: measure time millis

---
+++ src/fun/measureTimeMillis.ts
@@ -0,0 +1,16 @@
+
+export async function measureTimeMillis<T>(block: () => T | Promise<T>) {
+  const start = Date.now()
+  await block()
+  return Date.now() - start
+}
+
+export async function measureAndGetResult<T>(
+  block: () => T | Promise<T>,
+  onTime: (millis: number) => any
+) {
+  const start = Date.now()
+  const result = await block()
+  onTime(Date.now() - start)
+  return result
+}
