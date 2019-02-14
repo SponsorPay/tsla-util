2698eea7585f350787499038ec162cecdbeee1cb
Kobi <qobyyy@gmail.com>
2019-02-14 16:47:49 +0200

    feat: try catch (#20)

--- test/try.test.ts
+++ test/try.test.ts
@@ -1,4 +1,4 @@
-import {Try} from "../src"
+import {Try, tryCatch} from "../src"
 
 require("chai").should()
 
@@ -6,6 +6,26 @@ describe("try", function () {
   it("should getOrElse", () => {
     Try.of(() => "result").getOrElse(() => "bla").should.eq("result")
     Try.of(() => {
+      if (Math.random() > 0) {
+        throw Error()
+      }
+      return "result"
+    }).getOrElse(() => "bla").should.eq("bla")
+  })
+
+  it("should asOption", () => {
+    Try.of(() => "result").asOption.getOrElse(() => "bla").should.eq("result")
+    Try.of(() => {
+      if (Math.random() > 0) {
+        throw Error()
+      }
+      return "result"
+    }).asOption.getOrElse(() => "bla").should.eq("bla")
+  })
+
+  it("should tryCatch", () => {
+    tryCatch(() => "result").getOrElse(() => "bla").should.eq("result")
+    tryCatch(() => {
       if (Math.random() > 0) {
         throw Error()
       }
