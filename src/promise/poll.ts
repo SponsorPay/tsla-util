55f6802a703c700b8edb7f0ea431cea9cc500020
Kobi <qobyyy@gmail.com>
2019-02-19 12:37:53 +0200

    feat: poll dispose (#22)

--- src/promise/poll.ts
+++ src/promise/poll.ts
@@ -1,3 +1,5 @@
+import {defer, Defer} from "./defer"
+
 export interface PollOptions {
   delay?: number
   retries?: number
@@ -7,17 +9,60 @@ export class PollLimitError extends Error {
   name = "PollLimitError"
 }
 
-export function poll(runCheck: () => boolean | Promise<boolean>, {delay = 1000, retries = 50}: PollOptions = {}) {
-  return new Promise((resolve, reject) => {
+export class PollDisposedError extends Error {
+  name = "PollDisposedError"
+}
+
+export interface PollParams extends PollOptions {
+  runCheck: () => boolean | Promise<boolean>
+}
+
+export interface Poll {
+}
+
+export class Poll {
+  disposed = false
+  defer: Defer<void>
+  timeoutId?: number | object
+
+  constructor({runCheck, delay = 1000, retries = 50}: PollParams) {
+    this.defer = defer()
     const retry = async () => {
-      if (await runCheck()) {
-        resolve()
-      } else if (retries-- > 0) {
-        setTimeout(retry, delay)
+      if (!this.disposed) {
+        if (await runCheck()) {
+          this.defer.resolve()
+        } else if (retries-- > 0) {
+          this.timeoutId = setTimeout(retry, delay)
+        } else {
+          this.defer.reject(new PollLimitError())
+        }
       } else {
-        reject(new PollLimitError())
+        this.defer.reject(new PollDisposedError())
       }
     }
     retry()
-  })
+  }
+
+  get promise() {
+    return this.defer.promise
+  }
+
+  dispose() {
+    this.disposed = true
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId as number)
+      this.defer.reject(new PollDisposedError())
+    }
+  }
+}
+
+export function poll(
+  runCheck: () => boolean | Promise<boolean>,
+  {delay = 1000, retries = 50}: PollOptions = {},
+) {
+  return new Poll({
+    retries,
+    delay,
+    runCheck,
+  }).promise
 }
