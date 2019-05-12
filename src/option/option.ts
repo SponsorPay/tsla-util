4da891fdab8126b5c4447eb3c1c4155410de6455
kobi <kobi.b@inner-active.com>
2019-05-12 13:01:47 +0300

    feat(Option): orElse

--- src/option/option.ts
+++ src/option/option.ts
@@ -28,6 +28,10 @@ export abstract class Option<T> {
   flatMap<E>(fn: (t: T) => Option<E>): Option<E> {
     return this.isEmpty ? None.none as any : fn(this.get)
   }
+
+  orElse<E>(alternative: () => Option<E>): Option<E | T> {
+    return this.isEmpty ? alternative() : this
+  }
 }
 
 assignPropertyDescriptors(Some.prototype, Option.prototype)
