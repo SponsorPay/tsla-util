2f38e28f5b7041420fcb73a08fdca10fa1cf2893
kobi <kobi.b@inner-active.com>
2020-02-09 19:12:46 +0200

    parse map

--- src/parse/parse.ts
+++ src/parse/parse.ts
@@ -1,6 +1,10 @@
 import {Option} from "../option/option"
 import {optionOf} from "../option/optionOf"
 
+function isObject(e: unknown): e is object {
+  return e != null && typeof e === "object"
+}
+
 export function parseString(s: any) {
   return s != null ? String(s) : ""
 }
@@ -18,18 +22,18 @@ export function parseArray<T>(e: any, fn: (value: any) => T): T[] {
 }
 
 export function parseMap<K, V>(
-  e: any,
-  {parseKey, parseValue}: {parseKey: (k?: any) => K; parseValue: (v?: any) => V}
+  e: unknown,
+  {parseKey, parseValue}: {parseKey: (k?: unknown) => K; parseValue: (v?: unknown) => V}
 ): Map<K, V> {
-  if (e != null && typeof e === "object") {
-    return new Map(Object.keys(e).map(key => parseMapEntry([key, e[key]], {parseKey, parseValue})))
+  if (isObject(e)) {
+    return new Map(Object.keys(e).map(key => parseMapEntry([key, e[key as keyof typeof e]], {parseKey, parseValue})))
   }
   return new Map()
 }
 
 export function parseMapEntry<K, V>(
   entry: unknown,
-  {parseKey, parseValue}: {parseKey: (k?: any) => K; parseValue: (v?: any) => V}
+  {parseKey, parseValue}: {parseKey: (k?: unknown) => K; parseValue: (v?: unknown) => V}
 ): [K, V] {
   if (Array.isArray(entry)) {
     return [parseKey(entry[0]), parseValue(entry[1])]
